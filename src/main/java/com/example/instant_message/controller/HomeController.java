2d76e9b2d310381948d5779979050aa0e44efd46
AnhNguyen208 <nguyenletuananh20082001@gmail.com>
2023-06-02 14:46:55 +0700

    update home-page

--- src/main/java/com/example/instant_message/controller/HomeController.java
+++ src/main/java/com/example/instant_message/controller/HomeController.java
@@ -1,16 +1,13 @@
 package com.example.instant_message.controller;
 
-import com.example.instant_message.db.ConnectDB;
 import com.example.instant_message.model.User;
-import com.example.instant_message.service.UserService;
-import javafx.geometry.Insets;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import com.example.instant_message.ultils.Config;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.control.MenuButton;
-import javafx.scene.control.MenuItem;
+import javafx.geometry.Insets;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
@@ -19,10 +16,13 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
 public class HomeController extends BaseController implements Initializable {
+    @FXML
+    private Label title;
     @FXML
     private MenuButton menuButton;
     @FXML
@@ -30,24 +30,37 @@ public class HomeController extends BaseController implements Initializable {
     @FXML
     private MenuItem menuItem2;
     @FXML
+    private MenuItem menuItem3;
+    @FXML
     private VBox vBox;
-    private UserService userService;
+    @FXML
+    private TextField textFieldSearch;
+    @FXML
+    private Button btnSearch;
+    @FXML
+    private Label labelName;
+    @FXML
+    private Button btnCreateGroup;
 
     public HomeController(Stage stage, String screenPath) throws IOException {
         super(stage, screenPath);
-        BaseController.homeController = this;
     }
 
-    private void displayFriendList() throws SQLException {
-        List<User> userList = userService.friendList(BaseController.user.getId());
+    private void displayFriendList(List<User> userList) {
+        vBox.getChildren().clear();
         for (User user1 : userList) {
             HBox hBox = new HBox();
-            ImageView imageView = new ImageView(new Image("https://lh3.googleusercontent.com/75wmSt_ybBYi8OSc2JhdNH4iuYFstvkZ3JJxfE3u4b9AODEwmU20qMJkQzvI6WgicFV1xRBrF0sL-9XBaRop9cU2gqtiiTN_mGn_bg1GvP9a2TK8uVutAmy0_ZVlYFlmaeVLl-agyQ=w2400"));
+            ImageView imageView = new ImageView(new Image("https://lh3.googleusercontent.com/75wmSt_ybBYi8OSc2JhdNH4i" +
+                    "uYFstvkZ3JJxfE3u4b9AODEwmU20qMJkQzvI6WgicFV1xRBrF0sL-9XBaRop9cU2gqtiiTN_mGn_bg1GvP9a2TK8uVutAmy0_ZV" +
+                    "lYFlmaeVLl-agyQ=w2400"));
             imageView.setFitHeight(30);
             imageView.setFitWidth(50);
             HBox.setMargin(imageView, new Insets(0, 10, 10, 50));
             Label name = new Label(user1.getName());
             name.setFont(new Font("Arial", 24));
+            name.setOnMouseClicked(e -> {
+                labelName.setText(name.getText());
+            });
             hBox.getChildren().add(imageView);
             hBox.getChildren().add(name);
             vBox.getChildren().add(hBox);
@@ -56,23 +69,60 @@ public class HomeController extends BaseController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        userService = new UserService();
-        menuButton.setText("Xin chào, " + BaseController.user.getName());
-        menuItem1.setOnAction(e -> {
-            try {
-                ProfileController profileController = new ProfileController(this.stage, "/com/example/instant_message/profile.fxml");
-                profileController.show();
-            } catch (IOException ex) {
-                throw new RuntimeException(ex);
-            }
-        });
-        menuItem2.setOnAction(e -> {
-            BaseController.loginController.show();
-        });
         try {
-            displayFriendList();
+            labelName.setText("Chọn 1 người bạn hoặc 1 nhóm chat");
+            title.setText("Trang chủ");
+            menuButton.setText("Xin chào, " + BaseController.user.getName());
+            List <User> friendList = userService.friendList(BaseController.user.getId());
+            if(friendList != null) {
+                displayFriendList(friendList);
+            }
+
+            menuItem1.setOnAction(e -> {
+                try {
+                    ProfileController profileController = new ProfileController(this.stage, Config.PROFILE_SCREEN_PATH);
+                    profileController.show();
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            });
+            menuItem2.setOnAction(e -> {
+                try {
+                    FriendController friendController = new FriendController(this.stage, Config.FRIEND_SCREEN_PATH);
+                    friendController.show();
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            });
+            menuItem3.setOnAction(e -> {
+                try {
+                    LoginController loginController = new LoginController(this.stage, Config.LOGIN_SCREEN_PATH);
+                    loginController.show();
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            });
+            btnSearch.setOnMouseClicked(e -> {
+                String find = textFieldSearch.getText();
+                List<User> resultSearchList = new ArrayList<>();
+                for (User user : friendList) {
+                    if(user.getName().toLowerCase().contains(find.toLowerCase())) {
+                        resultSearchList.add(user);
+                    }
+                }
+                displayFriendList(resultSearchList);
+            });
+            btnCreateGroup.setOnMouseClicked(e -> {
+                try {
+                    CreateGroupController createGroupController = new CreateGroupController(new Stage(), Config.CREATE_GROUP_SCREEN_PATH);
+                    createGroupController.show();
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            });
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+
     }
 }
