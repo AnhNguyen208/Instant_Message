3504ee098d50e508d72410b49671fcf4d349bed9
AnhNguyen208 <nguyenletuananh20082001@gmail.com>
2023-06-02 14:42:56 +0700

    update config

---
+++ src/main/java/com/example/instant_message/ultils/Config.java
@@ -0,0 +1,12 @@
+package com.example.instant_message.ultils;
+
+public class Config {
+    public static final String SPLASH_SCREEN_PATH = "/com/example/instant_message/splash.fxml";
+    public static final String LOGIN_SCREEN_PATH = "/com/example/instant_message/login.fxml";
+    public static final String HOME_SCREEN_PATH = "/com/example/instant_message/home-page.fxml";
+    public static final String PROFILE_SCREEN_PATH = "/com/example/instant_message/profile.fxml";
+    public static final String FRIEND_SCREEN_PATH = "/com/example/instant_message/friend-page.fxml";
+    public static final String FRIEND_INFO_SCREEN_PATH = "/com/example/instant_message/friend-info.fxml";
+    public static final String CREATE_GROUP_SCREEN_PATH = "/com/example/instant_message/create-group.fxml";
+
+}
